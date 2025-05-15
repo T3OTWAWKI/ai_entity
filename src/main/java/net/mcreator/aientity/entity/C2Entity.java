921e1e2a2239e8c2db586165f361668f83643e72
adarsh <adarshveerapaneni@gmail.com>
2025-05-15 16:51:16 -0500

    small edit increasing follow range.

--- src/main/java/net/mcreator/aientity/entity/C2Entity.java
+++ src/main/java/net/mcreator/aientity/entity/C2Entity.java
@@ -138,7 +138,7 @@ public class C2Entity extends Monster {
 		builder = builder.add(Attributes.MAX_HEALTH, 10);
 		builder = builder.add(Attributes.ARMOR, 0);
 		builder = builder.add(Attributes.ATTACK_DAMAGE, 3);
-		builder = builder.add(Attributes.FOLLOW_RANGE, 16);
+		builder = builder.add(Attributes.FOLLOW_RANGE, 32);
 		return builder;
 	}
 
